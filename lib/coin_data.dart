943f8b446ab0eda57b5bd8a229270855bb9f30e5
shahrukh-sudo <57897724+shahrukh-sudo@users.noreply.github.com>
2020-09-29 18:36:35 +0530

    first commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,11 @@
+
+
+
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const apiKey = ' 6C5CBD80-8412-478A-8342-72BAF04CBD84';
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +36,22 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+class CoinData {
+
+
+
+  Future<String> getCoinData(String dat)async{
+
+    http.Response response = await http.get('https://rest.coinapi.io/v1/exchangerate/BTC/USD?apikey=$apiKey');
+    var data;
+
+    if (response.statusCode == 200){
+      data = jsonDecode(response.body);
+      return data['rate'].toString();
+    }
+    else{
+      print((response.statusCode));
+    }
+
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,11 @@
+
+import 'dart:io';
+
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,8 +13,66 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+
+
+  String selectCurrancy ='USD';
+  CoinData data =CoinData();
+
+String bitCoinValue = '?';
+  void getData()async{
+   String coin =await data.getCoinData(selectCurrancy);
+    setState(() {
+     bitCoinValue=coin;
+    });
+
+  }
+
+
+
+  DropdownButton<String> getDropDownButton() {
+
+    List<DropdownMenuItem<String>> dropdownItems =[];
+    for(int i =0; i< currenciesList.length; i++){
+      String currency = currenciesList[i];
+      var newItem = DropdownMenuItem(
+        child: Text(currency),
+        value: currency,
+      );
+      dropdownItems.add(newItem);
+
+    }
+    return DropdownButton<String> (
+        items: dropdownItems,
+        value: selectCurrancy ,
+        onChanged: (value){
+          setState(() {
+            selectCurrancy= value;
+            getData();
+          });
+        }
+    );
+  }
+
+
+  CupertinoPicker getCupertino(){
+    List<Text> getcupertinoItem = [];
+    for(int i=0;i<currenciesList.length;i++){
+      String data = currenciesList[i];
+
+      getcupertinoItem.add(Text(data));
+
+    }
+   return CupertinoPicker(itemExtent: 30, onSelectedItemChanged: (selectedIndex){
+      print(selectedIndex);
+    },
+      children: getcupertinoItem,
+    );
+
+  }
   @override
+
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
@@ -27,13 +92,13 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
+                '1 BTC = $bitCoinValue $selectCurrancy',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
                 ),
+              ),
               ),
             ),
           ),
@@ -42,10 +107,13 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
-          ),
+            child:Platform.isIOS?getCupertino() :getDropDownButton(),
+    ),
         ],
       ),
     );
   }
 }
+
+
+
